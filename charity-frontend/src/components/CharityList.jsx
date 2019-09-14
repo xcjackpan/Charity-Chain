b977a1f5c0ab549b1fc8c21dd6f5c152ba0b91e3
Hannah Wang <hannahwang12@gmail.com>
2019-09-14 00:04:04 -0400

    components

---
+++ charity-frontend/src/components/CharityList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class CharityList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Charity Category</h2>
+        <div>
+          charity list
+        </div>
+      </div>
+    );
+  }
+}
