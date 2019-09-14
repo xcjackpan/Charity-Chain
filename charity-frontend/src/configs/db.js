7bac36963ced9234127e70eab4e9d147be447b1d
brucejh99 <bjhong@yahoo.com>
2019-09-14 14:23:25 -0400

    Completed firebase db functions

--- charity-frontend/src/configs/db.js
+++ charity-frontend/src/configs/db.js
@@ -31,16 +31,18 @@ export const getSpecificCharity = (uid) => {
     });
 }
 
-export const addTransactionToUser = (uid, public_id) => {
-    db.ref(`users/${uid}/transaction/${public_id}`).push(public_id);
+export const addTransactionToUser = (uid, public_id) =>
+  db.ref(`users/${uid}/transaction/${public_id}`).push(public_id);
+
+export const getRefOfTransactions = (uid) => {
+  return db.ref(`users/${uid}/transaction`).once('value')
+    .then(snapshot => {
+      return snapshot.val();
+    });
 }
 
-export const getRefOfTransactions = (uid) =>
-    db.ref(`users/${uid}/transactions`);
-
-export const consumeTransactionFromUser = (uid, public_id) => {
-    db.ref(`users/${uid}/consumed_transaction/${public_id}`).push(public_id);
-}
+export const consumeTransactionFromUser = (uid, public_id) =>
+  db.ref(`users/${uid}/consumed_transaction/${public_id}`).push(public_id);
 
 //export const upvoteImage = (uid, public_id) => {
 //  db.ref(`users/${uid}/upvoted/${public_id}`).push(public_id);
@@ -51,21 +53,36 @@ export const consumeTransactionFromUser = (uid, public_id) => {
 //  });
 //}
 
-export const getRefOfConsumedTransactions = (uid) =>
-  db.ref(`users/${uid}/consumed_transactions`);
+export const getRefOfConsumedTransactions = (uid) => {
+  return db.ref(`users/${uid}/consumed_transaction`).once('value')
+    .then(snapshot => {
+      return snapshot.val();
+    });
+}
 
-export const doCreateCharity = (id, account_number, email, image) =>
+export const incrementDonation = (uid) => {
+  const dbRef = db.ref(`charities/${uid}/donation_count`);
+  dbRef.transaction(donation_count => {
+    if(donation_count || donation_count === 0) {
+      donation_count = donation_count + 1;
+    }
+    return donation_count;
+  });
+}
+
+export const doCreateCharity = (id, account_number, email, image, donation_count = 0) =>
   db.ref('charities').push({
     public_id: id,
     account_number,
     email,
     image,
+    donation_count
   });
 
 export const doCreateUser = (id, username, email) => {
   let favourites = {placeholder: 'empty_child'};
   let uploaded = {placeholder: 'empty_child'}
-  let upvoted = {placeholder: 'empty_child' }
+  let upvoted = {placeholder: 'empty_child'}
   return db.ref(`users/${id}`).set({
     favourites,
     uploaded,
