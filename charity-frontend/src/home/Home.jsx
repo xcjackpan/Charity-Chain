33ca63a744eea31aa7afc56c4651a19fcccd5b03
Hannah Wang <hannahwang12@gmail.com>
2019-09-14 16:18:30 -0400

    sort and navigate buttons

--- charity-frontend/src/home/Home.jsx
+++ charity-frontend/src/home/Home.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import _ from 'lodash';
-import { Radio, Select } from 'antd';
+import { Select } from 'antd';
+import { User} from 'react-feather'
 import CharityList from './CharityList';
 import { getRefOfCharities } from '../configs/db.js';
 import './Home.css';
@@ -31,15 +33,14 @@ export default class Home extends React.Component {
     return (
       <div className="home">
         <h1>CharityChain</h1>
-        <Radio.Group onChange={this.changeSort} defaultValue="popular">
-          <Radio.Button value="popular">Popular</Radio.Button>
-          <Radio.Button value="spending">Spending</Radio.Button>
-        </Radio.Group>
-        {this.state.sort === 'spending' &&
-        <Select defaultValue="food">
-          <Select.Option value="food">Food</Select.Option>
-          <Select.Option value="retail">Retail</Select.Option>
-        </Select>}
+        <Link to="/profile"><div><User /><span>User Profile</span></div></Link>
+        <Select defaultValue="popular" onChange={this.changeSort} style={{ width: 150 }}>
+          <Select.Option value="popular">Popular</Select.Option>
+          <Select.OptGroup label="Spending Category">
+            <Select.Option value="food">Food</Select.Option>
+            <Select.Option value="retail">Retail</Select.Option>
+          </Select.OptGroup>
+        </Select>
         {Object.keys(this.state.charities)
           .map(category => <CharityList category={category} charities={this.state.charities[category]} sort={this.state.sort} />)}
       </div>
--- charity-frontend/src/profile/ProfileView.jsx
+++ charity-frontend/src/profile/ProfileView.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link} from 'react-router-dom';
 import { Row, Col } from 'antd';
+import { Grid } from 'react-feather'
 import CharityBox from './CharityBox';
 import './ProfileView.css';
 
@@ -7,12 +9,16 @@ export default class ProfileView extends Component {
     render() {
         const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
         return (
-            <div className='profile-charities-container'>
-                {/*data.forEach(charity => <CharityBox />)*/}
-                <CharityBox />
-                <CharityBox />
-                <CharityBox />
+            <div>
+                <Link to="/"><div><Grid /><span>Browse Charities</span></div></Link>
+                <div className='profile-charities-container'>
+                    {/*data.forEach(charity => <CharityBox />)*/}
+                    <CharityBox />
+                    <CharityBox />
+                    <CharityBox />
+                </div>
             </div>
+            
         )
     }
 }
