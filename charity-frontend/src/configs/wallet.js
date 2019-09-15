061a061acc931c137ca1d814db238ee4babf7a28
Aphsai <saksham.a@hotmail.ca>
2019-09-14 20:21:13 -0400

    updated api

--- charity-frontend/src/configs/wallet.js
+++ charity-frontend/src/configs/wallet.js
@@ -1,12 +1,27 @@
 const { link } = require('@blockmason/link-sdk');
 
-const wallet = link({
-    clientId: 'btDP8g92RzLvGvwK95jYeVvasGVze0XHAB4XDfCo0Nc',
-    clientSecret: 'L+o7i4GOXvwOlND+E5n3oz4Jrq4GSYHBOBCne6P7An/BA2JYEhSJWiI4j7dtKL9'
+const chain = link({
+    clientId: '',
+    clientSecret: ''
 }, {
     fetch
 });
 
-export { wallet };
+export const getBalance = (address) =>
+    chain.get('/getBalanceOfWallet', { token_holder : address });
 
+export const depositMoney = (amount_to_deposit, receiver) =>
+    chain.get('/depositMoney', { amountToDeposit : amount_to_deposit, receiver : receiver });
+
+export const sendToCharity = (amount_to_send, sender, receiver) =>
+    chain.get('/sendToCharity', { amount_spent : amount_to_send, sender : sender, receiver : receiver, senderTimestamped : sender + (new Date().getTime()) });
+
+export const reimburseTransaction = (amount_to_send, charity_address, td_transaction_record, category) => 
+    chain.get('/reimburseTransaction', { amount_spent : amount_to_send, charity : charity_address, td_transaction_record : td_transaction_record, category : category });
+
+export const getAllDeposits = () =>
+    chain.get('/events/Deposit');
+
+export const getAllReimbursements = () =>
+    chain.get('/events/Reimbursement');
 
