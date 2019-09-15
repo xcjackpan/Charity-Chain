12dd0cfb7479945210c5eb4d7bdf9317a98f0418
brucejh99 <bjhong@yahoo.com>
2019-09-15 00:00:23 -0400

    idk

--- charity-frontend/src/profile/ProfileContainer.jsx
+++ charity-frontend/src/profile/ProfileContainer.jsx
@@ -29,18 +29,25 @@ export default class ProfileContainer extends Component {
       })
       .then(uid => {
         db.getRefOfTransactions(uid)
-          .then(res => Object.keys(res).map(key => res[key])
-            .map(transaction => ({
-              ...transaction,
-              timestamp: new Date(transaction.timestamp)
-            }))
-            .sort((a, b) => b.timestamp - a.timestamp))
-          .then(res => this.setState({ transactions: res }));
+          .then(res => {
+            
+            let transactions_of_user =  Object.keys(res).map(key => res[key])
+            wallet.getAllReimbursements().then(res => {
+              let user_reimbursements = res.data.filter((e) => uid !== e.reimburseFrom.split("@")[0]);
+              for(let i = 0; i < transactions_of_user.length; i++) {
+                console.log(user_reimbursements.filter(e => e.reimburseFrom.split('@')[1] === transactions_of_user[i].timestamp));
+              }
+              console.log(user_reimbursements);
+            });
+          })
+            // .sort((a, b) => b.timestamp - a.timestamp))
+          // .then(res => this.setState({ transactions: res }));
         })
   }
 
   render() {
-    wallet.getAllReimbursements().then((res) => console.log(res));
+    // wallet.reimburseTransaction(10, '0x78b9b2b62571760a013053bd27b4c805fad7715c', '123', 'SHOPPING').then(res => console.log(res));
+    wallet.getBalance("0x78b9b2b62571760a013053bd27b4c805fad7715c").then(res => console.log(res));
     return (
         <ProfileView { ...this.props } transactions={this.state.transactions} />
     );
